788e6b17931b9c9723c395d511feee7d12eff2f6
MinYss <saocwxx116@naver.com>
2022-11-25 15:38:23 +0900

    !

---
+++ lab01_intro/src/edu/java/intro1/AppMain1.java
@@ -0,0 +1,26 @@
+//주석(comment) : 프로그램에 대한 설명을 작성.
+//주석은 컴파일할 때 제외됨.(실행되지 않는 코드)
+// inLine comment
+
+/* 
+ * block comment - 엔터를 칠 때다가 주석처리됨.
+ *               - 문서화 주석(documentation comment)를 만들 때
+ */ 
+
+package edu.java.intro1; //패키지 선언문
+
+//클래스 선언(정의)
+public class AppMain1 {
+
+	//main 메서드(정의) : 자바 프로그램의 시작과 끝.
+	public static void main(String[] args) {
+		System.out.println("안녕하세요. 여러분!"); // 모든 문장이 끝날때 세미콜론(;)을 꼭 써줘야 한다.
+		
+		System.out.println("Hello, 자바!");
+	} // end main 
+	
+
+} // end class
+
+
+
---
+++ lab01_intro/src/edu/java/intro2/AppMain2.java
@@ -0,0 +1,20 @@
+package edu.java.intro2;
+
+public class AppMain2 {
+
+	public static void main(String[] args) {
+		System.out.println("자바 프로그램 실행 방법");
+		System.out.println("Menu -> Run -> Run");
+		System.out.println("Ctrol+F11");
+		//문자열(string) : 문자들의 집합(sequence). ("")를 사용.
+		
+		//Ctrol+space: 코드 추천 및 자동 완성.
+		
+		System.out.println("문자열" + "합치기"); //문자열을 이어주는 연산자.
+		System.out.println(2+1); //덧셈 연산자
+		System.out.println("1+2="+(1+2));
+		System.out.print("프린트");
+		System.out.print("print");
+	}
+
+}
